d23770764454bb61a7087180f2572ff3493b8363
Albert <ksk836684@gmail.com>
2024-09-02 21:42:24 +0400

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 
+import HeaderCmp from "./components/HeaderCmp.vue";
 </script>
 
 <template>
-  
+  <div>
+    <header-cmp/>
+    <main>
+      <router-view/>
+    </main>
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/HeaderCmp.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <header class="flex-center">
+    <router-link to="/" exact>Главная</router-link>
+    <router-link to="/convert" exact>Конвертация</router-link>
+  </header>
+</template>
+
+<style scoped lang="scss">
+
+header {
+  gap: 5rem;
+  padding: 2rem 0;
+
+  a {
+    font-size: 1.8rem;
+    transition: .2s color;
+
+    &:hover {
+      color: #4941e3;
+    }
+  }
+}
+
+</style>
