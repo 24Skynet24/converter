41ee78a58d6d68893c11a7bfa4e9db61a0e66b2d
Albert <ksk836684@gmail.com>
2024-09-03 16:06:39 +0400

    add Home content

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,37 @@
 <script lang="ts" setup>
 
+import ExchangeCmp from "../components/ExchangeCmp.vue";
 </script>
 
 <template>
-
+  <section class="home_section flex-center">
+    <h1>exchange rate</h1>
+    <ul>
+      <li>
+        <exchange-cmp :currencyName="'RUB'" :currencyVal="101"/>
+      </li>
+    </ul>
+  </section>
 </template>
+
+<style scoped lang="scss">
+
+.home_section {
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  padding-top: 10rem;
+
+  h1 {
+    text-transform: uppercase;
+    margin-bottom: 5rem;
+    font-size: 3.2rem;
+  }
+
+  ul {
+    display: flex;
+    flex-direction: column;
+  }
+}
+
+</style>
