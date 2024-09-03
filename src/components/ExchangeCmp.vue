8b57aa4d7a3e802fc430c9d84ce6449d14bb1e96
Albert <ksk836684@gmail.com>
2024-09-03 16:01:12 +0400

    add Exchange component

---
+++ src/components/ExchangeCmp.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import {defineProps} from "vue";
+
+defineProps<{
+  currencyName: string
+  currencyVal: number
+}>()
+
+</script>
+
+<template>
+  <article class="align-center">
+    <span>1 USD</span>
+    <span>=</span>
+    <div class="align-center">
+      <span>{{ currencyVal }}</span>
+      <span>{{ currencyName }}</span>
+    </div>
+  </article>
+</template>
+
+<style lang="scss" scoped>
+
+article {
+  gap: 2rem;
+  font-size: 1.8rem;
+
+  div.align-center {
+    gap: .5rem;
+  }
+}
+
+</style>
