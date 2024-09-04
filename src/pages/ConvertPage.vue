a1c7527670e6291bfc22622d4af85788236700b6
Albert <ksk836684@gmail.com>
2024-09-04 20:09:46 +0400

    add converter

--- src/pages/ConvertPage.vue
+++ src/pages/ConvertPage.vue
@@ -1,5 +1,50 @@
 <script lang="ts" setup>
-const currenciesName = ["USD", "EUR", "RUB"]
+import {ref} from "vue"
+import {currencyStore} from "../store/index"
+
+const useStore = currencyStore()
+const currencyList = useStore.currencyList
+const currenciesName = ["usd", "eur", "rub"]
+const firstItem = ref({
+  activeCurrency: 0,
+  value: "",
+})
+const secondItem = ref({
+  activeCurrency: 1,
+  value: "",
+})
+
+const changeCurrencyValue = (order = true) => {
+  const key = Object.keys(currencyList).filter(item => {
+    const [name1, name2] = item.split("-")
+    return name1 === currenciesName[firstItem.value.activeCurrency] && name2 === currenciesName[secondItem.value.activeCurrency]
+  })[0]
+
+  if (order) secondItem.value.value = (+firstItem.value.value * currencyList[key]).toFixed(2) + ""
+  else firstItem.value.value = (+secondItem.value.value / currencyList[key]).toFixed(2) + ""
+}
+
+const changeCurrency = (id, order = true): void => {
+  if (order) {
+    if (secondItem.value.activeCurrency === id) return
+    firstItem.value.activeCurrency = id
+  }
+  else {
+    if (firstItem.value.activeCurrency === id) return
+    secondItem.value.activeCurrency = id
+  }
+  changeCurrencyValue(order)
+}
+const changeValue = (value, order = true): void => {
+  if (order) {
+    secondItem.value.value = value
+  }
+  else {
+    firstItem.value.value = value
+  }
+  changeCurrencyValue(order)
+}
+
 </script>
 
 <template>
@@ -7,25 +52,25 @@ const currenciesName = ["USD", "EUR", "RUB"]
     <form class="flex-column">
       <div class="convert_item align-center">
         <div class="convert_item__drop">
-          <span>{{ currenciesName[0] }}</span>
+          <span>{{ currenciesName[firstItem.activeCurrency] }}</span>
           <ul class="flex-column">
-            <li v-for="name in currenciesName" :key="`currenciesName__${name}`">
+            <li v-for="(name, id) in currenciesName" :key="`currenciesName__${name}`" @click="changeCurrency(id)">
               <span>{{ name }}</span>
             </li>
           </ul>
         </div>
-        <input class="convert_item__val" type="number">
+        <input class="convert_item__val" type="number" v-model="firstItem.value" @input="changeValue($event.target.value)">
       </div>
       <div class="convert_item align-center">
         <div class="convert_item__drop">
-          <span>{{ currenciesName[0] }}</span>
+          <span>{{ currenciesName[secondItem.activeCurrency] }}</span>
           <ul class="flex-column">
-            <li v-for="name in currenciesName" :key="`currenciesName__${name}`">
+            <li v-for="(name, id) in currenciesName" :key="`currenciesName__${name}`" @click="changeCurrency(id, false)">
               <span>{{ name }}</span>
             </li>
           </ul>
         </div>
-        <input class="convert_item__val" type="number">
+        <input class="convert_item__val" type="number" v-model="secondItem.value" @input="changeValue($event.target.value, false)">
       </div>
     </form>
   </section>
@@ -49,6 +94,11 @@ const currenciesName = ["USD", "EUR", "RUB"]
 
     span {
       cursor: pointer;
+      text-transform: uppercase;
+    }
+
+    input {
+      padding: .5rem 1rem;
     }
 
     &__drop {
